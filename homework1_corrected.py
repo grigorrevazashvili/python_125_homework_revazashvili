408599ccfd5602786dd3f977715b8f53b0d3d928
Grigor <grigorrevazashvili@gmail.com>
2026-04-20 20:34:32 +0400

    Add homework 1 corrected

---
+++ homework1_corrected.py
@@ -0,0 +1,70 @@
+#Task 1
+
+first_name="Grigor"
+last_name="Revazashvili"
+age=31
+height=1.85
+city="Tbilisi"
+country="Georgia"
+job_title="Sales Manager"
+is_student=True
+favorite_language="English"
+
+print(f"First name:{first_name}\nLast name:{last_name}\nAge:{age}\nHeight:{height}\nCity:{city}\nCountry:{country}\nJob title:{job_title}\nIs student:{is_student}\nFavorite language:{favorite_language}")
+
+#Task 2
+
+#case 1
+total_price=150.0
+tax=total_price*1.12
+print(tax)
+
+#case 2
+total_price=150.0
+discount=total_price-30
+print(discount)
+
+#case 3
+total_price=150
+split=total_price/3
+print(split)
+
+#case 4
+print(split)
+
+#Task 3
+
+temperature=38.5
+normal_temperature=36.6
+print(temperature>normal_temperature)
+print(temperature==normal_temperature)
+print(temperature!=normal_temperature)
+print(temperature<=40)
+
+#Task 4
+
+a=42
+print(type(a))
+
+b=3.14
+print(type(b))
+
+c="Hello"
+print(type(c))
+
+d=True
+print(type(d))
+
+#Task 5
+
+x=17
+y=5
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
